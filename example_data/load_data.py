f1a8edccfc0b6b941a77964f9311ce2519967893
Yixuan Sun <yixuan@Yixuans-Mac-mini.local>
2021-08-16 10:14:48 -0400

    fixed bug

--- example_data/load_data.py
+++ example_data/load_data.py
@@ -2,15 +2,17 @@ import glob
 import numpy as np
 import pandas as pd
 
-def load_example_data():
-    test_data_dir = './set0/'
+def load_example_data(dir_path):
+    test_data_dir = dir_path
     files = glob.glob(test_data_dir +'*.csv')
+    print("Loading file %s" % files)
     currents = []
     cell_pot = []
     capacity = []
     energy = []
     power = []
     for f in files:
+        print("Loading file %s" % f)
         temp = pd.read_csv(f)
         currents.append(temp['Curr den (A/m^2)'].values[0])
         cell_pot.append(temp['Cell pot(V)'].values)
@@ -20,3 +22,4 @@ def load_example_data():
 
     idx_ = np.argsort(currents)
     return np.array(cell_pot)[idx_], np.array(capacity)[idx_], np.array(currents)[idx_], np.array(power)[idx_], np.array(energy)[idx_]
+
